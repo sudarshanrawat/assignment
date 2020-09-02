4bf0e0da36b847e6005bad5398c3f25d1dacc682
sudarshanrawat <sudarshanrawat7701@gmail.com>
2020-09-02 17:30:15 +0530

    palindrome string

---
+++ oops-assign3/Q10.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+bool testPalindrome(string isPallandrome, int start , int last)
+{
+  if (start == last)
+  {
+    return true;
+  }
+
+  if ( isPallandrome[start] != isPallandrome[last])
+  {
+    return false;
+  }
+  if (start < last)
+    testPalindrome(isPallandrome, start + 1, last - 1);
+
+  return true;
+}
+int main()
+{
+  string isPallandrome;
+  cout<<"enter the string: "<<endl;
+  getline(cin, isPallandrome);
+  int last =  isPallandrome.size();
+  int start = 0;
+  cout << testPalindrome(isPallandrome, start, last - 1);
+  return 0;
+}
